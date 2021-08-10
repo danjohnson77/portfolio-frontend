5f469f041d1825cdf3bfdacd8b18045035920ccb
Daniel Johnson <danjohnson77@gmail.com>
2021-08-10 16:36:11 -0700

    added basic page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,44 @@ import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps }) {
+import { SwitchTransition, Transition } from "react-transition-group";
+import gsap from "gsap";
+
+const MyApp = ({ Component, pageProps, router }) => {
+  const enter = (node) => {
+    gsap.from(node, {
+      duration: 0.3,
+      autoAlpha: 0,
+      x: window.innerWidth,
+      ease: "power1.out",
+    });
+  };
+
+  const exit = (node) => {
+    gsap.to(node, {
+      duration: 0.2,
+      autoAlpha: 0,
+      x: -window.innerWidth,
+      ease: "power1.in",
+    });
+  };
   return (
     <Layout>
-      <Component {...pageProps} />
+      <SwitchTransition>
+        <Transition
+          key={router.pathname}
+          timeout={500}
+          in={true}
+          onEnter={enter}
+          onExit={exit}
+          mountOnEnter={true}
+          unmountOnExit={true}
+        >
+          <Component {...pageProps} />
+        </Transition>
+      </SwitchTransition>
     </Layout>
   );
-}
+};
 
 export default MyApp;
