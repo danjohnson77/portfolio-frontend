088fbecb4689abbf7fc253b0477db655e2620da3
Daniel Johnson <danjohnson77@gmail.com>
2021-08-16 16:06:23 -0700

    added loading component

---
+++ components/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen w-screen">
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,36 @@ import Layout from "../components/Layout";
 
 import { SwitchTransition, Transition } from "react-transition-group";
 import gsap from "gsap";
+import { useEffect, useState } from "react";
+import Loading from "../components/Loading";
 
 const MyApp = ({ Component, pageProps, router }) => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const handleStart = () => {
+      setLoading(true);
+    };
+    const handleComplete = () => {
+      setLoading(false);
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
+
   const enter = (node) => {
     gsap.from(node, {
       duration: 0.3,
       autoAlpha: 0,
       x: window.innerWidth,
-      ease: "power1.out",
+      ease: "circ.out",
     });
   };
 
@@ -20,7 +42,7 @@ const MyApp = ({ Component, pageProps, router }) => {
       duration: 0.2,
       autoAlpha: 0,
 
-      ease: "power1.in",
+      ease: "circ.in",
     });
   };
   return (
@@ -35,7 +57,7 @@ const MyApp = ({ Component, pageProps, router }) => {
           mountOnEnter={true}
           unmountOnExit={true}
         >
-          <Component {...pageProps} />
+          {loading ? <Loading /> : <Component {...pageProps} />}
         </Transition>
       </SwitchTransition>
     </Layout>
